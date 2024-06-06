be12bc2ee77526fa4d87f99100516d3e948f1a3b
Dr.Sudhamoy Debbarma <65818001+sdmydbr9@users.noreply.github.com>
2024-06-06 19:04:36 +0530

    Update MMM-ModuleScheduler.js

--- MMM-ModuleScheduler.js
+++ MMM-ModuleScheduler.js
@@ -101,34 +101,33 @@ Module.register("MMM-ModuleScheduler", {
 		}
 	},
 
-	setModuleDisplay: function (module, action, brightness) {
-		const options = this.config.uselock ? { lockString: this.identifier } : "";
-		Log.log(this.name + " is processing the " + action + (action === "DIM_MODULE" ? " (" + brightness + "%)" : "") + " request for " + module.identifier);
-
-		if (action === "SHOW_MODULE") {
-			module["show"](
-				this.config.animationSpeed,
-				() => {
-					Log.log(this.name + " has shown " + module.identifier);
-					this.setModuleBrightness(module.identifier, 100);
-				},
-				options
-			);
-			return true;
-		}
-
-		if (action === "HIDE_MODULE") {
-			module.hide(this.config.animationSpeed, Log.log(this.name + " has hidden " + module.identifier), options);
-			return true;
-		}
-
-		if (action === "DIM_MODULE") {
-			this.setModuleBrightness(module.identifier, brightness);
-			return true;
-		}
-
-		return false;
-	},
+	setModuleDisplay: function(module, action, brightness) {
+        const options = this.config.uselock ? { lockString: this.identifier } : "";
+        Log.log(this.name + " is processing the " + action + (action === "DIM_MODULE" ? " (" + brightness + "%)" : "") + " request for " + module.identifier);
+    
+        if (action === "SHOW_MODULE") {
+            module.show(this.config.animationSpeed, { animation: "fade" }, () => {
+                Log.log(this.name + " has shown " + module.identifier);
+                this.setModuleBrightness(module.identifier, 100);
+            }, options);
+            return true;
+        }
+    
+        if (action === "HIDE_MODULE") {
+            module.hide(this.config.animationSpeed, { animation: "fade" }, () => {
+                Log.log(this.name + " has hidden " + module.identifier);
+            }, options);
+            return true;
+        }
+    
+        if (action === "DIM_MODULE") {
+            this.setModuleBrightness(module.identifier, brightness);
+            return true;
+        }
+    
+        return false;
+    },
+    
 	setModuleBrightness(moduleIdentifier, brightness = 100) {
 		const moduleDiv = document.getElementById(moduleIdentifier);
 		if (moduleDiv) {
